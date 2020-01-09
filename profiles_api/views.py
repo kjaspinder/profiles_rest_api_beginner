53248ee3e257219805215f9c16816758668b72e6
Manish Mahajan <androiddev@purpletealabs.com>
2020-01-09 17:33:45 +0400

    added helloapiview

--- profiles_api/views.py
+++ profiles_api/views.py
@@ -1,3 +1,16 @@
-from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
 
-# Create your views here.
+class HelloApiView(APIView):
+    """test api view """
+
+    def get(self,request,format=None):
+        """ return the list of apiview features"""
+        an_apiview =[
+        'uses http methods as function (get,post,patch,put,delete)',
+        'is similar to traditional django view ',
+        'gives you the most control over your app logic',
+        'is mapped manually to URLs'
+        ]
+        """return a dictionery in response which will be converted to json when response is delivered"""
+        return Response({'message':'Hello!','an_apiview':an_apiview})
